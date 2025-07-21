e22bf2d4a5cbe6b47beacd53992d0c06223b3995
wsudswong <wsudswon@uwaterloo.ca>
2025-07-21 11:39:38 -0400

    wrap buttons in NookCard

--- infinethy-app/app/page.tsx
+++ infinethy-app/app/page.tsx
@@ -23,10 +23,10 @@ export default function Home() {
         <h1 className="text-3xl font-bold text-white dark:text-black">Hello, I&apos;m {name}</h1>
         <div className="flex">
           {/* <button className="w-32 flex-1 bg-indigo-500 hover:bg-indigo-700"><a href="/badexample">Bad Example</a></button> */}
-          <Link href="/habitracker"><button className="w-32 flex-1 bg-sky-500 hover:bg-sky-700">Habitracker</button></Link>
+          <Link href="/habitracker"><button className="w-32 flex-1 bg-sky-500 hover:bg-sky-700"><NookCard>Habitracker</NookCard></button></Link>
           <Link href="/params/272"><button className="w-32 flex-1 bg-fuchsia-500 hover:bg-fuchsia-700"><NookCard>Params</NookCard></button></Link>
           <Link href="/habitracker"><button className="w-32 flex-1 bg-emerald-500 hover:bg-emerald-700"><NookCard>HowlingHabitracker</NookCard></button></Link>
-          <Link href="/params/dynamic"><button className="w-32 flex-1 bg-red-400 hover:bg-red-700">React Playground</button></Link>
+          <Link href="/params/dynamic"><button className="w-32 flex-1 bg-red-400 hover:bg-red-700"><NookCard>React Playground</NookCard></button></Link>
         </div>
         <div className="p-4">
           <div className="flow-root text-white dark:text-black">Welcome to my headpsace! Feel free to have a seat in any 
