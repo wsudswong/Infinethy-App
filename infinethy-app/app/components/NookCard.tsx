35fa6fb655222fb1153cc3815220b602adb00c59
wsudswong <wsudswon@uwaterloo.ca>
2025-07-21 11:25:13 -0400

    pass children in NookCard

--- infinethy-app/app/components/NookCard.tsx
+++ infinethy-app/app/components/NookCard.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Howls from './Howls'
-import Link from 'next/link'
 
-const NookCard = () => {
+const NookCard = ({children}: {children: React.ReactNode}) => {
   return (
     // empty elements are called "Fragments" the allow one return element but are not elements
     <>
       <div>
-          <Link href="../habitracker"><Howls /></Link>
+          <p>{children}</p>
+          <Howls />
       </div>
     </>
   )
--- infinethy-app/app/page.tsx
+++ infinethy-app/app/page.tsx
@@ -26,8 +26,7 @@ export default function Home() {
           <Link href="/habitracker"><button className="w-32 flex-1 bg-sky-500 hover:bg-sky-700">Habitracker</button></Link>
           <Link href="/params/272"><button className="w-32 flex-1 bg-fuchsia-500 hover:bg-fuchsia-700">Params</button></Link>
           {/* TODO: Full button and howl */}
-          {/* <div className="w-32 flex-1 bg-emerald-500 hover:bg-emerald-700"><NookCard /></div> */}
-          <Link href="/habitracker"><button className="w-32 flex-1 bg-emerald-500 hover:bg-emerald-700"><NookCard></NookCard></button></Link>
+          <Link href="/habitracker"><button className="w-32 flex-1 bg-emerald-500 hover:bg-emerald-700"><NookCard>HowlingHabitracker</NookCard></button></Link>
           <Link href="/params/dynamic"><button className="w-32 flex-1 bg-red-400 hover:bg-red-700">React Playground</button></Link>
         </div>
         <div className="p-4">
